cf229c5761528129b88e5e29a9ca121dc87a5acc
Poguri Diya <diya>
2025-10-31 16:37:13 +0530

    Removed nested git repo

---
+++ terraform/count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = ["mongodb","mysql","redis"]
+}
+
+variable "zone_id" {
+    default = "Z09362922S2ZXJ8DP3JCV"
+}
+
+variable "domain_name" {
+    default = "poguri.fun"
+}
