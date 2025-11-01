e4a46be88b4287cd581590fa955df47ef475d6e1
Poguri Diya <diya>
2025-11-02 00:00:26 +0530

    terraform

---
+++ locals/data.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "joindevops" {
+    owners = ["973714476881"]
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name = "root-device-type"
+        values = ["ebs"]
+    }
+}
+
+    output "ami_id" {
+        value = data.aws_ami.joindevops.id
+    }
+
